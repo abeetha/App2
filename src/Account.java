ac68debb48d393e6fa567d3b40256ff53a41f662
Abeetha <abeethaabeysundera@gmail.com>
2022-10-12 15:12:51 +0530

    1

---
+++ src/Account.java
@@ -0,0 +1,40 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    private long accountId;
+
+    private String type;
+
+    @OneToOne()
+    private Customer customer;
+
+    public Account() {
+    }
+
+    public Account(long accountId, String type) {
+        this.accountId = accountId;
+        this.type = type;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
